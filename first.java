4edabae6b77b08c8e98a2985b28c0e482b1642c5
Sumi Kumari <sumiK9784@gmail.com>
2025-01-07 11:24:34 +0530

    Your commit message

---
+++ first.java
@@ -0,0 +1,44 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 5; // Change this number for a different multiplication table
+        
+        System.out.println("Multiplication Table for " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
+
+
+
+
+
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; // Numbers <= 1 are not prime
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // Found a divisor
+            }
+        }
+        return true; // No divisors found
+    }
+}
